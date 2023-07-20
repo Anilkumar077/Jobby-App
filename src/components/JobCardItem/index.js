aeed4ded730f1cd2e15fdcce2d3bd92d726b0e3b
Anilkumar077 <banilkumar0508@gmail.com>
2023-07-20 10:35:36 +0000

    Jobby App

---
+++ src/components/JobCardItem/index.js
@@ -0,0 +1,68 @@
+import {Link} from 'react-router-dom'
+
+import {BsBriefcaseFill} from 'react-icons/bs'
+import {MdLocationOn} from 'react-icons/md'
+import {AiFillStar} from 'react-icons/ai'
+
+import './index.css'
+
+const JobCardItem = props => {
+  const {item} = props
+  const {
+    companyLogoUrl,
+    employmentType,
+    id,
+    jobDescription,
+    location,
+    packagePerAnnum,
+    rating,
+    title,
+  } = item
+
+  return (
+    <>
+      <Link to={`/jobs/${id}`} className="link">
+        <li className="job-item-container">
+          <div className="first-part-container">
+            <div className="img-title-container">
+              <img
+                src={companyLogoUrl}
+                alt="company logo"
+                className="company-logo"
+              />
+              <div className="title-rating-container">
+                <h1 className="title-heading">{title}</h1>
+                <div className="star-rating-container">
+                  <AiFillStar className="star-icon" />
+                  <p className="rating-text">{rating}</p>
+                </div>
+              </div>
+            </div>
+            <div className="location-package-container">
+              <div className="location-job-type-container">
+                <div className="location-icon-container">
+                  <MdLocationOn className="location-icon" />
+                  <p className="location">{location}</p>
+                </div>
+                <div className="employment-type-icon-container">
+                  <BsBriefcaseFill />
+                  <p className="job-type">{employmentType}</p>
+                </div>
+              </div>
+              <div>
+                <p className="package">{packagePerAnnum}</p>
+              </div>
+            </div>
+          </div>
+          <hr className="item-hr-line" />
+          <div className="second-part-container">
+            <h1 className="description-heading">Description</h1>
+            <p className="description-para">{jobDescription}</p>
+          </div>
+        </li>
+      </Link>
+    </>
+  )
+}
+
+export default JobCardItem
